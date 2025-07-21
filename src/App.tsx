11fe0a1a0da63c2e68a6c9cacb3a5b1079fbdc5a
yaaabs <brianyabut02@gmail.com>
2025-07-21 19:45:06 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, I'm YabuTech</h1>
+      <p>I'm going to build a portfolio using React + Vite.</p>
+    </div>
+  );
+}
+
+export default App;
+
